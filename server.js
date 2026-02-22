2a7db362e1870f8f6c1a9f98268a54533285cd55
lecharles <charleslozano@gmail.com>
2026-02-21 17:11:57 -0800

    Add home view and .gitignore (in earlier commit)

--- server.js
+++ server.js
@@ -23,3 +23,8 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => {
     res.send("test route working");
 });
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
