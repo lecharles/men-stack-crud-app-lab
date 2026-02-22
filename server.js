6120961122257d646c1b18f4fc9c29142db2fee3
lecharles <charleslozano@gmail.com>
2026-02-21 17:48:08 -0800

    Connect to MongoDB Atlas

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ require("dotenv").config(); // load .env file and add env vars to process.env
 // import dependencies
 const express = require("express");  
 const methodOverride = require("method-override"); // bc html forms support get & post only. it allows put and delete methods
+const mongoose = require("mongoose");
 
 const app = express(); // create
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+
+// log connection status
+mongoose.connection.on("connected", () => { 
+    // .connection baked in with mongoose. listens for "connected" event and runs callback function when event emitted
+    console.log(`Connected... to MongoDB and to the model: ${mongoose.connection.name}`);
+});
 
 // middleware
 app.use(express.urlencoded({ extended: false })); // parse html form data into req.body
