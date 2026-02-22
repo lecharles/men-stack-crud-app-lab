65ac8cbb7ea77f4d3a213893c4c4c614c15187eb
lecharles <charleslozano@gmail.com>
2026-02-21 17:01:30 -0800

    Remore cors. Add middleware

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ console.log("MEN stack crud app lab started...");
 require("dotenv").config(); // load .env file and add env vars to process.env
 
 // import dependencies
-const express = require("express"); 
-const cors = require("cors"); 
+const express = require("express");  
 const methodOverride = require("method-override"); // bc html forms support get & post only. it allows put and delete methods
 
-const app = express(); // create an express app
+const app = express(); // create
+
+// middleware
+app.use(express.urlencoded({ extended: false })); // parse html form data into req.body
+app.use(methodOverride("_method")); // allow form to send PUT and DELETE requests
